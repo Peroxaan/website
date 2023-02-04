a9683b07d1c3f770692350bb6a926b05e0d06e85
Nicholas Christopher <nchristopher@tuta.io>
2023-02-03 22:09:52 -0800

    🐛 Fix off-centered app icon

--- pages/[app].js
+++ pages/[app].js
@@ -19,7 +19,7 @@ export default function App({ app }) {
 							width="96"
 							height="96"
 							alt={`${app.name} app icon`}
-							className="mr-4 h-24 w-24 rounded-md"
+							className="h-24 w-24 rounded-md md:mr-4"
 						/>
 						<div>
 							<h1 className="mb-2 mt-2 text-4xl font-bold md:mt-0">
